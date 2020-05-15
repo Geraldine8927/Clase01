5113f0caefe80beeb6602fa8dcc4854542c1bc54
Geraldine8927 <geraldinevillar@gmail.com>
2020-05-14 20:30:35 -0500

    clase01 android studio con kotlin

---
+++ app/src/main/java/com/example/clase01/ConfirmarMenu.kt
@@ -0,0 +1,17 @@
+package com.example.clase01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_confirmar_menu.*
+
+class ConfirmarMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmar_menu)
+
+        edtDetalleImpresion.setText(intent.extras!!["DetallePedido"]!!.toString())
+        edtCostoImpresion.setText(intent.extras!!["Costo"]!!.toString())
+        edtCantidadImpresion.setText(intent.extras!!["Cantidad"]!!.toString())
+    }
+}
---
+++ app/src/main/java/com/example/clase01/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.clase01
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+     var Costo:Double=0.0
+     var Cantidad:Double=0.0
+    var Total:Double=0.0
+    var Descuento=0.0
+    var CostoDelivery:Double=0.0
+    var TotalPagar:Double=0.0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun CalcularTotales():Boolean
+
+    {
+        if(EdtCosto.text.isEmpty() || EdtCosto.text.equals("0"))
+        {
+            Toast.makeText(baseContext,"por favor ingresar la cantidad",Toast.LENGTH_LONG).show()
+            return false
+        }
+        Costo=EdtCosto.text.toString().toDouble()
+        Cantidad=EdtCantidad.text.toString().toDouble()
+        Total=Costo*Cantidad
+
+        if (Total>300)
+        {
+            Descuento=Total*0.10
+        }
+    if (chkDelivery.isChecked)
+    {
+        CostoDelivery=20.0
+    }
+        TotalPagar=(Total-Descuento)+CostoDelivery
+        return true;
+}
+    public fun btnCalcularClick(v:View)
+    {
+        if(CalcularTotales())
+        {
+            EdtTotal.setText(Total.toString())
+            EdtDescuento.setText(Descuento.toString())
+            EdtTotalPagar.setText(TotalPagar.toString())
+        }
+
+    }
+    public fun btnImprimir_OnClick(v:View)
+    {
+        EdtTotal.setText(Total.toString())
+        EdtDescuento.setText(Descuento.toString())
+        EdtTotalPagar.setText(TotalPagar.toString())
+
+        var oVentaImprimir:Intent=Intent(this,ConfirmarMenu::class.java)
+        oVentaImprimir.putExtra("DetallePedido",EdtDetallePedido.text)
+        oVentaImprimir.putExtra("Costo",EdtCosto.text)
+        oVentaImprimir.putExtra("Cantidad",EdtCantidad.text)
+        oVentaImprimir.putExtra("Total",EdtTotal.text)
+        oVentaImprimir.putExtra("Descuento",EdtDescuento.text)
+        oVentaImprimir.putExtra("Delivery",CostoDelivery.toString())
+        oVentaImprimir.putExtra("Totalpagar",EdtTotalPagar.toString())
+        startActivity(oVentaImprimir)
+
+    }
+
+
+}
+
